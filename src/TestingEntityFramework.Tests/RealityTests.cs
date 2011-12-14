37e3d2566fb2eeeb458d4558ea0406b5ab78156d
NotMyself <bobby.johnson@gmail.com>
2011-12-14 07:06:02 -0800

    setting up the initial project structure

---
+++ src/TestingEntityFramework.Tests/RealityTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestingEntityFramework.Tests
+{
+    [TestFixture]
+    public class RealityTests
+    {
+        [Test]
+        public void true_should_be_true()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
