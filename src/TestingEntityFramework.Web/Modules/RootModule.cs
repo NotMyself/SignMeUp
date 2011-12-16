831f061a55317a182c88cd395f05af14a19180c9
NotMyself <bobby.johnson@gmail.com>
2011-12-16 10:02:09 -0800

    added a nancy site

---
+++ src/TestingEntityFramework.Web/Modules/RootModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace TestingEntityFramework.Web.Modules
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            Get["/"] = x => View["index"];
+        }
+    }
+}
